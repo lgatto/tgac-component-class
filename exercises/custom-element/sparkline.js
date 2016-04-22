0d13387f9962d5da14224a10fa676843136d1a8a
Laurent <lg390@cam.ac.uk>
2016-04-22 15:56:32 +0100

    inline sparklines

--- exercises/custom-element/sparkline.js
+++ exercises/custom-element/sparkline.js
@@ -2,7 +2,7 @@ const d3 = require("d3");
 
 module.exports.render = function({
     el = required("el"),
-    width = 50,
+    width = 100,
     height = 20,
     color = "black",
     points = [],
@@ -10,8 +10,10 @@ module.exports.render = function({
 
     // points = [{x:10, y:20}, {x:20, y:25}, {x:80, y:95}, {x:90, y:111}, {x:100, y:20}, {x:110, y:50}];
 
+    const X_PADDING = 2;
+    
     const xScale = d3.scale.linear()
-	      .range([0, width])
+	      .range([X_PADDING, width - X_PADDING])
 	      .domain(d3.extent(points, (d) => d.x));
 
     const yScale = d3.scale.linear()
@@ -20,12 +22,12 @@ module.exports.render = function({
     
     // function makeLine(points: Array,{x: number, y: number}>: svgPathString {}
     const makeLine = d3.svg.line()
+	      .x(({x}) => xScale(x))
+	      .y(({y}) => yScale(y))
     
-	  .x(({x}) => xScale(x))
-	  .y(({y}) => yScale(y))
-    
-    const update = d3.select("el")
-	  .data([points])
+    const update = d3.select(el)
+	      .selectAll("svg")
+	      .data([points])
 
     update.enter()
 	.append("svg")
@@ -43,28 +45,23 @@ module.exports.render = function({
 	    stroke: color
 	})
 
-    // const updatePoint = d3.select(el)
-    // 	      .select("svg")
-    // 	      .selectAll("circle")
-    // 	      .data(points)
+    const updatePoint = d3.select(el)
+    	      .select("svg")
+    	      .selectAll("circle")
+    	      .data(points)
 
-    // const enterPoint = updatePoint
-    // 	      .enter()
-    // 	      .append("circle")
-    // 	      .attr("r", width/20)
-    // 	      .attr("fill", "black")
+    const enterPoint = updatePoint
+    	      .enter()
+    	      .append("circle")
+    	      .attr("r", width/50)
+    	      .attr("fill", "black")
 
-    // updatePoint.attr("cx", (d) => xScale(d.x))
-    //     .attr("cy", (d) => yScale(d.y))
-    //     .attr("title", (d) => {
-    //         return "you clicked: " + JSON.stringify(d);
-    //     })
-    //     .on("click", (d) => {
-    //         alert("you clicked: " + JSON.stringify(d));
-    //     })
-    
-
-    
+    updatePoint.attr("cx", (d) => xScale(d.x))
+        .attr("cy", (d) => yScale(d.y))
+        .on("click", (d) => {
+            alert("you clicked: " + JSON.stringify(d));
+        })
+        
 }
 
 

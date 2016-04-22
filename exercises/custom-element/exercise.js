188497721a95be1015b1539cd547b97a870bab9c
Laurent <lg390@cam.ac.uk>
2016-04-22 15:17:03 +0100

    sparklines

--- exercises/custom-element/exercise.js
+++ exercises/custom-element/exercise.js
@@ -1,16 +1,85 @@
+const sparklineVis = require("./sparkline");
 
-// TODO create your element prototype - make sure you use
-// Object.create() to inherit
+class Sparkline extends HTMLElement {
+    createdCallback() {
+	this._observed = ["width", "height", "color", "points"];
+	this.style.display = "inline-block"; 
+    }
 
-// TODO implement the createdCallback to setup the
-// element
+    attachedCallback() {
+	this._takeAllAttributes();
+	this._render();
+    }
 
-// TODO implement the attachedCallback to create
-// our visual content
-//
-// TODO implement the detachedCallback to remove
-// our visual content
+    attributeChanged(name, previous, current) {
+	this[name] = current;
+    }
 
-// TODO implement attributeChanged to handle
-// parsing, and then give this to our d3 code
+    // this.points = ...
+    set points(string) {
+	const values = string.split(" ")
+	      .map(s => parseFloat(s));
+	this._points = values;
+    }
 
+    set color(v) {
+	this._color = v || "black";
+    }
+
+    // const = this.points;
+    // document.querySelector("spark-line").points
+    get points() {
+	return this._points;
+    }
+    
+    _takeAllAttributes() {
+	for (const attribute of this._observed) {
+	    this[attribute] = this.getAttribute(attribute);
+	}
+    }
+    
+    _render() {
+
+	const points = this._points.map(function(y, i) {
+	    return {y, x: i};
+	})
+	
+	sparklineVis.render({
+	    el: this,
+	    width: this._width,
+	    height: this._height,
+	    color: this._color,
+	    points: points,
+	})
+    }
+}
+
+document.registerElement("spark-line", Sparkline);
+
+// // TODO create your element prototype - make sure you use
+// // Object.create() to inherit - HTMLElement
+
+// const sparklinePt = Object.create(HTMLElement.prototype);
+
+
+// // TODO implement the createdCallback to setup the
+// // element
+
+// sparklinePt.createdCallback = function() {
+//     this.innerHTML = "I am a sparkline";
+//     this.style.color = "red";
+// }
+
+// // TODO implement the attachedCallback to create
+// // our visual content
+// //
+// // TODO implement the detachedCallback to remove
+// // our visual content
+
+// // TODO implement attributeChanged to handle
+// // parsing, and then give this to our d3 code
+
+// // TODO register 
+// document.registerElement("spark-line", {
+//   prototype: sparklinePt,
+// });
---
+++ exercises/custom-element/sparkline.js
@@ -0,0 +1,73 @@
+const d3 = require("d3");
+
+module.exports.render = function({
+    el = required("el"),
+    width = 50,
+    height = 20,
+    color = "black",
+    points = [],
+}) {
+
+    // points = [{x:10, y:20}, {x:20, y:25}, {x:80, y:95}, {x:90, y:111}, {x:100, y:20}, {x:110, y:50}];
+
+    const xScale = d3.scale.linear()
+	      .range([0, width])
+	      .domain(d3.extent(points, (d) => d.x));
+
+    const yScale = d3.scale.linear()
+	      .range([height - 2, 2])
+	      .domain(d3.extent(points, (d) => d.y));
+    
+    // function makeLine(points: Array,{x: number, y: number}>: svgPathString {}
+    const makeLine = d3.svg.line()
+    
+	  .x(({x}) => xScale(x))
+	  .y(({y}) => yScale(y))
+    
+    const update = d3.select("el")
+	  .data([points])
+
+    update.enter()
+	.append("svg")
+	.append("path")
+
+    // update + enter
+    update
+	.attr("width", width + "px")
+	.attr("height", height + "px")
+
+    update.select("path")
+	.attr("d", makeLine)
+	.attr({
+	    fill: "none",
+	    stroke: color
+	})
+
+    // const updatePoint = d3.select(el)
+    // 	      .select("svg")
+    // 	      .selectAll("circle")
+    // 	      .data(points)
+
+    // const enterPoint = updatePoint
+    // 	      .enter()
+    // 	      .append("circle")
+    // 	      .attr("r", width/20)
+    // 	      .attr("fill", "black")
+
+    // updatePoint.attr("cx", (d) => xScale(d.x))
+    //     .attr("cy", (d) => yScale(d.y))
+    //     .attr("title", (d) => {
+    //         return "you clicked: " + JSON.stringify(d);
+    //     })
+    //     .on("click", (d) => {
+    //         alert("you clicked: " + JSON.stringify(d));
+    //     })
+    
+
+    
+}
+
+
+function required(name) {
+    throw Error(`missing option: ${name}`);
+}
